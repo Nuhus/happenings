1a838b66410084ca8299f7b7df05c712cdf33491
sadiq <link2nuhu@gmail.com>
2022-08-22 15:22:19 +0100

    updated database connection string

--- utils/dbConnection.jsx
+++ utils/dbConnection.jsx
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose'
 const connectDb = async () =>{
-await mongoose.connect("mongodb://localhost:27017/newsdb")
+await mongoose.connect("mongodb://Nsadiq:Sadiq57563@cluster0-shard-00-00.xjthd.mongodb.net:27017,cluster0-shard-00-01.xjthd.mongodb.net:27017,cluster0-shard-00-02.xjthd.mongodb.net:27017/?ssl=true&replicaSet=atlas-qa727v-shard-0&authSource=admin&retryWrites=true&w=majority")
+
 }
 export default connectDb
